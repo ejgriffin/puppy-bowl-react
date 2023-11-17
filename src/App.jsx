20f8ac6b336bf0473ee4e986c28d6cbab7dd2022
Eric Griffin <ejgriffin13@gmail.com>
2023-11-16 19:39:18 -0500

    installed react router and set up Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AllPlayers from "./components/AllPlayers";
+import SinglePlayer from "./components/SInglePlayer";
+import "./App.css";
+
 function App() {
-  return <>PUPPY BOWL 2!!</>;
+  return (
+    <BrowserRouter>
+      <h1>PUPPY BOWL 2!!</h1>
+      <Routes>
+        <Route path="/" element={<AllPlayers />} />
+        <Route path="/player" element={<SinglePlayer />} />
+        <Route path="*" element={<AllPlayers />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/SInglePlayer.jsx
@@ -0,0 +1,5 @@
+import { Link } from "react-router-dom";
+
+export default function SinglePlayer() {
+  return <Link to="/">Single Player</Link>;
+}
