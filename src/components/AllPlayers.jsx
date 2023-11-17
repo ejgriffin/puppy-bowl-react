a6ad21492b6d9a126165ef286ee1c7562a789147
Eric Griffin <ejgriffin13@gmail.com>
2023-11-16 21:03:33 -0500

    added CSS

--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,3 +1,9 @@
+//Fetch All Player Data
+//Render All Players in div with image, player name, details button, and delete player button
+//details button should link to SinglePlayer component
+//Add form for adding new puppy player with input for name and breed
+//Add Search Bar with ability to Search for player. (Google it)
+
 export default function AllPlayers() {
   return <>All Players</>;
 }
--- src/components/SInglePlayer.jsx
+++ src/components/SInglePlayer.jsx
@@ -1,3 +1,6 @@
+//create div to render single player data that shows image, name, breed, and status
+//add button that returns to AllPlayers component
+
 import { Link } from "react-router-dom";
 
 export default function SinglePlayer() {
